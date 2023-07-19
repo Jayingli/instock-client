bad3e3b50e6f06066cf4a65f59d0e1606adf1ed0
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-07-19 16:52:33 -0400

    - [x]  Get warehouseList component to be visible through npm start to facilitate styling - [x]  Build out scss foundation based on classes given in the sketch - [x]  basic styling - to be updated once variables are available - [x]  re-factor warehouse__list-item card as a table per the tablet/desktop views

--- src/components/warehouseList/warehouseList.jsx
+++ src/components/warehouseList/warehouseList.jsx
@@ -1,44 +1,78 @@
-import { Link } from 'react-router-dom';
-import Warehouse from '../../Pages/warehouse/warehouse';
+import { Link } from "react-router-dom";
+import Warehouse from "../../Pages/warehouse/warehouse";
+import "../warehouseList/warehouseList.scss";
 
 //Warehouse List Component
 
 function WarehouseList() {
+  return (
+    <div className="warehouse__list--component">
+      <h1>Warehouses</h1>
+      <input type="text" name="search" placeholder="Search..."></input>
+      <button>+Add New Warehouse</button>
 
-    return(
-        <div className="warehouse__list">
-            <h1>Warehouses</h1>
-            <input><img></img></input>
-            <button>+Add New Warehouse</button>
-            
-            <ul>
-                {/* Map function to generate cards based on server information */}
+      <div className="warehouse__list">
+        {/* Map function to generate cards based on server information */}
 
-                {/* Example card */}
-                <div className="warehouse__list--item">
-                    <h4>Warehouse</h4>
-                    <Link to={Warehouse}></Link>  {/*replace Warehouse with Warehouse Inventory page when built*/}
+        {/* Example card */}
+        <div className="warehouse__list--item">
+          <h4>Warehouse</h4>
 
-                    <h4>Contact Name</h4>
-                    <p>Parmin Aujla</p>
+          <h4>Contact Name</h4>
 
-                    <h4>Address</h4>
-                    <p>503 Broadway, New York, USA</p>
+          <a href="">Manhattan</a>
+          {/* <Link to={Warehouse}></Link>  replace Warehouse with Warehouse Inventory page when built */}
 
-                    <h4>Contact Information</h4>
-                    <p>+1 (629) 555-0129</p>
-                    <a href="mailto: paujla@instock.com">paujla@instock.com</a>    
+          <p>Parmin Aujla</p>
 
-                    <img src="" alt="Delete Warehouse Button" />
-                    <img src="" alt="Edit Warehouse Button" />
+          <h4>Address</h4>
 
-                </div>
+          <h4>Contact Information</h4>
 
+          <p className="address">503 Broadway, New York, USA</p>
 
-            </ul>
-
+          <p>+1 (629) 555-0129</p>
+          <a href="mailto: paujla@instock.com">paujla@instock.com</a>
+          <div className="button__wrap">
+            <img src="" alt="Delete Warehouse Button" />
+            <img src="" alt="Edit Warehouse Button" />
+          </div>
         </div>
-    )
+
+        <div className="warehouse__list--item">
+          <table>
+            <tr>
+              <th>Warehouse</th>
+              <th>Contact Name</th>
+              <th>Address</th>
+              <th>Contact Information</th>
+              {/* <th>Actions</th> */}
+                {/*To be hidden on mobile*/}
+
+            </tr>
+
+            <tr>
+              <td><a href="">Manhattan</a></td>
+              {/* <Link to={Warehouse}></Link>  replace Warehouse with Warehouse Inventory page when built */}
+
+              <td><p>Parmin Aujla</p></td>
+
+              <td><p className="address">503 Broadway, New York, USA</p></td>
+
+              <td><p>+1 (629) 555-0129</p></td>
+
+              <td><a href="mailto: paujla@instock.com">paujla@instock.com</a></td>
+
+              <td className="button__wrap">
+                <img src="" alt="Delete Warehouse Button" />
+                <img src="" alt="Edit Warehouse Button" />
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WarehouseList;
