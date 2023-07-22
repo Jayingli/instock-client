af0907b283551974f8c2bc882679a4e75f758447
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-07-22 10:59:50 -0400

    - [x]  duplicate existing table - [x]  refactor to match mobile set up - [x]  rename tables to reflect which view they’re for - [x]  try to set up new classnames to be hidden in specfic views - [x]  add email into previous td - [x]  fix css structure to reconnect the styling - [x]  add dynamic colour to button

--- src/components/warehouseList/warehouseList.jsx
+++ src/components/warehouseList/warehouseList.jsx
@@ -15,7 +15,7 @@ function WarehouseList({warehouseData}) {
       <input type="text" name="search" placeholder="Search..."></input>
       <button>+Add New Warehouse</button>
 
-      <div className="warehouse__list">
+      <div className="warehouse__list--mobile">
         {/* Map function to generate cards based on server information */}
 
 
@@ -28,10 +28,6 @@ function WarehouseList({warehouseData}) {
                   <tr>
                     <th>Warehouse</th>
                     <th>Contact Name</th>
-                    <th>Address</th>
-                    <th>Contact Information</th>
-                    {/* <th>Actions</th> */}
-                      {/*To be hidden on mobile*/}
       
                   </tr>
                 </thead>
@@ -42,25 +38,82 @@ function WarehouseList({warehouseData}) {
       
                     <td><p>{warehouse.contact_name}</p></td>
       
+                  </tr>
+                </tbody>
+                <thead>
+                  <tr>
+                    <th>Address</th>
+                    <th>Contact Information</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
                     <td><p className="address">{warehouse.address}</p></td>
-      
-                    <td><p>{warehouse.contact_phone}</p></td>
-      
-                    <td><a href={`mailto: ${warehouse.contact_email}`}>{warehouse.contact_email}</a></td>
-      
+                    <td><p>{warehouse.contact_phone}</p>
+                    <a href={`mailto: ${warehouse.contact_email}`}>{warehouse.contact_email}</a></td>
+                  </tr>
+                  <tr>
                     <td className="button__wrap">
-                      <img src="" alt="Delete Warehouse Button" />
-                      <img src="" alt="Edit Warehouse Button" />
-                    </td>
+                        <img src="" alt="Delete Warehouse Button" />
+                        <img src="" alt="Edit Warehouse Button" />
+                      </td>
+
                   </tr>
                 </tbody>
             </table>
   
             )
           })}
-        </div>
+        </div></div>
+
+
+    <div className="warehouse__list--tabdesc">
+      {/* Map function to generate cards based on server information */}
+
+
+      <div className="warehouse__list--item">
+        {warehouseData.map(warehouse => {
+
+          return(
+            <table>
+              <thead>
+                <tr>
+                  <th>Warehouse</th>
+                  <th>Contact Name</th>
+                  <th>Address</th>
+                  <th>Contact Information</th>
+                  {/* <th>Actions</th> */}
+                    {/*To be hidden on mobile*/}
+    
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><a href="">{warehouse.warehouse_name}</a></td>
+                  {/* <Link to={Warehouse}></Link>  replace Warehouse with Warehouse Inventory page when built */}
+    
+                  <td><p>{warehouse.contact_name}</p></td>
+    
+                  <td><p className="address">{warehouse.address}</p></td>
+    
+                  <td><p>{warehouse.contact_phone}</p></td>
+    
+                  <td><a href={`mailto: ${warehouse.contact_email}`}>{warehouse.contact_email}</a></td>
+    
+                  <td className="button__wrap">
+                    <img src="" alt="Delete Warehouse Button" />
+                    <img src="" alt="Edit Warehouse Button" />
+                  </td>
+                </tr>
+              </tbody>
+          </table>
+
+          )
+        })}
       </div>
     </div>
+  </div>
+
   );
 }
 
