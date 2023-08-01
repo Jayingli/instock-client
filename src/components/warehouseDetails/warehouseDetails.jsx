bd6346469eeb6b684167a348381d7ad29ba320f3
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-08-01 15:42:24 -0400

    add Link to back arrow tweak styling to adjust for change

--- src/components/warehouseDetails/warehouseDetails.jsx
+++ src/components/warehouseDetails/warehouseDetails.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import "../warehouseDetails/warehouseDetails.scss";
 import axios from "axios";
 import { useParams } from "react-router";
-import editIcon from '../../assets/icons/edit-24px.svg';
-import backArrow from '../../assets/icons/arrow_back-24px.svg';
+import editIcon from "../../assets/icons/edit-24px.svg";
+import backArrow from "../../assets/icons/arrow_back-24px.svg";
 import { Link } from "react-router-dom";
 
 //WarehouseDetails Component
@@ -36,54 +36,59 @@ function WarehouseDetails() {
   return (
     <div className="warehouse__details">
       {details.map((warehouse) => {
+        if (warehouse.id == id) {
+          return (
+            <div>
+              <div className="title__wrap">
+                <div className="warehouse">
+                  <Link to="/warehouses"><img src={backArrow} alt="Back Arrow" />
+                  </Link>
+                  <h1>{warehouse.warehouse_name}</h1>
+                </div>
+                <div className="edit">
+                  <Link to="/warehouses/:id">
+                    <img src={editIcon} alt="edit icon" fill="white" />
+                    <p>Edit</p>
+                  </Link>
+                </div>
+              </div>
 
-        if (warehouse.id == id){
-
-            return (
-                <div>
-                <div className="title__wrap">
-                    <div className="warehouse">
-                      <img src={backArrow} alt="Back Arrow" />
-                      <h1>{warehouse.warehouse_name}</h1>
-                    </div>
-                    <Link to="/warehouses/:id"><img src={editIcon} alt="edit icon" fill="white" /><p>Edit</p></Link>
+              <div className="detail__wrap">
+                <div className="detail__header--mobile">
+                  <h4>Warehouse Address:</h4>
+                  <p>
+                    {warehouse.address}, {warehouse.city}, {warehouse.country}
+                  </p>
                 </div>
 
-                <div className="detail__wrap">
-                    <div className="detail__header--mobile">
-                      <h4>Warehouse Address:</h4>
-                      <p>{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
-                    </div>
-
-                    <div className="detail__header--tabdesc">
-                      <h4>Warehouse Address:</h4>
-                      <p>{warehouse.address}, </p>
-                      <p>{warehouse.city}, {warehouse.country}</p>
-                    </div>
-
-
-                    <div className="contact__wrap">
-                    <div className="name__wrap">
-                        <h4>Contact Name:</h4>
-                        <p>{warehouse.contact_name}</p>
-                        <p>{warehouse.contact_position}</p>
-                    </div>
-
-                    <div className="info__wrap">
-                        <h4>Contact Information:</h4>
-                        <p>{warehouse.contact_phone}</p>
-                        <a href={`mailto: ${warehouse.contact_email}`}>{warehouse.contact_email}</a>
-                    </div>
-                    </div>
+                <div className="detail__header--tabdesc">
+                  <h4>Warehouse Address:</h4>
+                  <p>{warehouse.address}, </p>
+                  <p>
+                    {warehouse.city}, {warehouse.country}
+                  </p>
                 </div>
-                </div>
-            )
-          }
-      }
-      )
-    }
 
-      
+                <div className="contact__wrap">
+                  <div className="name__wrap">
+                    <h4>Contact Name:</h4>
+                    <p>{warehouse.contact_name}</p>
+                    <p>{warehouse.contact_position}</p>
+                  </div>
+
+                  <div className="info__wrap">
+                    <h4>Contact Information:</h4>
+                    <p>{warehouse.contact_phone}</p>
+                    <a href={`mailto: ${warehouse.contact_email}`}>
+                      {warehouse.contact_email}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }
