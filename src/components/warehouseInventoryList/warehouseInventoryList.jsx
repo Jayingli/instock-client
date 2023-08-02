52adac62db937e9f9ed2048f030e50b9072a488a
Sehaj Wadhwa <104042287+sehajwadhwa@users.noreply.github.com>
2023-08-01 20:54:04 -0400

    merging branch

--- src/components/warehouseInventoryList/warehouseInventoryList.jsx
+++ src/components/warehouseInventoryList/warehouseInventoryList.jsx
@@ -4,8 +4,15 @@ import "../warehouseInventoryList/warehouseInventoryList.scss";
 //Warehouse Inventory List Component
 
 function WarehouseInventoryList() {
-  const { id } = useParams();
-  console.log(id);
+  // const { id } = useParams();
+  // useEffect () = {
+  //   // to fetch warehouse data
+  //   // 5050
+  //   const url = "https://localhost:5050/api/warehouses"
+  //   axios.get(url)
+
+  //   }
+  // console.log(id);
   return (
     <div>
       <div>
