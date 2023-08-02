23db54bb777f28ebb0c6ecae63cdcfea23dc8045
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-08-02 11:53:48 -0400

    - [x]  Make new branch for j2p1-23 - [x]  Create component jsx - [x]  create component scss - [x]  Sketch out component structure with dummy info - [x]  Sketch out css structure - [x]  Add delete component to bottom of inventories page so you can style more easily - [x]  Basic styling for mobile - [x]  add media queries - [x]  basic styling for tablet - [x]  basic styling for desktop

--- src/Pages/inventory/inventory.jsx
+++ src/Pages/inventory/inventory.jsx
@@ -1,6 +1,6 @@
 import "../inventory/inventory.scss";
 import InventoryList from "../../components/inventoryList/inventoryList";
-
+import DeleteItem from "../../components/deleteItem/deleteItem";
 
 //Inventory Page
 
@@ -9,6 +9,7 @@ function Inventory() {
     return (
         <section id="inventory">
             <InventoryList />
+            <DeleteItem />
         </section>
     )
 }
