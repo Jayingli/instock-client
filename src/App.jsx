3945ec9e3c2cb9d46076d6a1ca999e4ba6b2c2ea
ckatieng <123335732+ckatieng@users.noreply.github.com>
2023-08-03 16:32:03 -0700

    Revert "Alisaj2p1 9"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-// import WarehousesPage from './pages/WarehousesPage/WarehousesPage';
-// import InventoryPage from './pages/InventoryPage/InventoryPage';
+import WarehousesPage from './pages/WarehousesPage/WarehousesPage';
+import InventoryPage from './pages/InventoryPage/InventoryPage';
 import './App.scss';
-import Warehouse from './Pages/warehouse/warehouse';
-import Inventory from './Pages/inventory/inventory';
-import WarehouseDetails from './components/warehouseDetails/warehouseDetails';
+
 /*
  * App.jsx
  * - Represents the main component of the application
@@ -23,22 +21,22 @@ function App() {
                 {/* Define routes for different pages */}
                 <Routes>
                     {/* Home Page */}
-                    <Route path="/" element={<Warehouse />} />
+                    <Route path="/" element={<Navigate to="/warehouses" />} />
 
                     {/* Warehouses Page */}
-                    {/* <Route path="/warehouses" element={<WarehousesPage />} /> */}
+                    <Route path="/warehouses" element={<WarehousesPage />} />
 
                     {/* Single Warehouse Page */}
-                    <Route path="/warehouses/:id" element={<WarehouseDetails />} />
+                    {/* <Route path="/warehouses/:id" element={<WarehousesPage />} /> */}
 
                     {/* Inventories of Individual Warehouse Page*/}
                     {/* <Route path="/warehouses/:id/inventories" element={<WarehousesPage />} /> */}
 
                     {/* Inventory Page */}
-                    <Route path="/inventories" element={<Inventory />} />
+                    <Route path="/inventories" element={<InventoryPage />} />
 
                     {/* Single Inventory Page */}
-                    <Route path="/inventories/:id" element={<Inventory />} />
+                    {/* <Route path="/inventories/:id" element={<InventoryPage />} /> */}
 
                     {/* Catch-all to redirect to Home Page */}
                     <Route path="*" element={<Navigate to="/" />} />
