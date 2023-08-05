e136cc586a8177b25835ed51f801c928d85dc066
Gabriela <lopes.gb18@gmail.com>
2023-08-05 17:28:50 -0400

    axios-set-up

--- src/components/warehouseAdd/warehouseAdd.jsx
+++ src/components/warehouseAdd/warehouseAdd.jsx
@@ -1,13 +1,16 @@
 import "./warehouseAdd.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import backArrow from "../../assets/icons/arrow_back-24px.svg";
 import Button from "../Button/Button";
 import { useState } from "react";
+import axios from "axios";
 
 const WarehouseAdd = () => {
   //Variables
   const baseURL = process.env.REACT_APP_BASE_URL;
+  let navigate = useNavigate();
 
+  console.log(baseURL);
   //States
   const [warehouseState, setWarehouseState] = useState({
     warehouse_name: "",
@@ -32,12 +35,25 @@ const WarehouseAdd = () => {
     contact_email,
   } = warehouseState;
 
+  //Function to add dashes to phone number
+
   //onChange handle
   const handleOnChange = (event) => {
     const value = event.target.value;
+    const nameForm = event.target.name;
+    const valuePhoneDash = value.replace(/^(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+
+    //If the state change is contact_phone, it will modify the phone number by adding dashes, so the server can accept it
+    if (nameForm === "contact_phone") {
+      return setWarehouseState({
+        ...warehouseState,
+        [nameForm]: valuePhoneDash,
+      });
+    }
+
     setWarehouseState({
       ...warehouseState,
-      [event.target.name]: value,
+      [nameForm]: value,
     });
   };
 
@@ -63,41 +79,69 @@ const WarehouseAdd = () => {
 
   //Phone Number validation
   const isPhoneValid = () => {
-const phoneValidator = /1?-?\(?[0-9]{3}[\-\)][0-9]{3}-[0-9]{4}/;
+    const phoneValidator = /1?-?\(?[0-9]{3}[\-\)][0-9]{3}-[0-9]{4}/;
 
     if (!phoneValidator.test(contact_phone)) {
       return false;
     } else {
       return true;
-    };
+    }
   };
 
-//   Email Validation
-const isEmailValid = ()=>{
-    if (!contact_email.includes("@")){
-        return false;
+  //   Email Validation
+  const isEmailValid = () => {
+    if (!contact_email.includes("@")) {
+      return false;
     } else {
-        return true;
-    };
-};
+      return true;
+    }
+  };
 
-// Final validation
-const isValidForm = () =>{
-    if(!isEmailValid){
-        return false;
-    };
-    if(!isPhoneValid){
-        return false;
-    };
-    if(!isEmailValid){
-        return false;
-    };
+  // Final validation
+  const isValidForm = () => {
+    if (!isFieldsValid()) {
+      return false;
+    }
+    if (!isEmailValid()) {
+      return false;
+    }
+    if (!isPhoneValid()) {
+      return false;
+    }
+    if (!isEmailValid()) {
+      return false;
+    }
     return true;
-}
+  };
 
   //Handle to submit form
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (isValidForm()) {
+      axios
+        .post(`${baseURL}/api/warehouses`, {
+          warehouse_name: warehouse_name,
+          address: address,
+          city: city,
+          country: country,
+          contact_name: contact_name,
+          contact_position: contact_position,
+          contact_phone: contact_phone,
+          contact_email: contact_email,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      //Once form is submitted, re-direct to Warehouses List page
+      navigate('/warehouses');
+    } else {
+    console.log("Form Invalid")
+    }
   };
 
   return (
