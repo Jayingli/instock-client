0b43318c808a72144259e52866ad378fdc4374c7
Sehaj Wadhwa <104042287+sehajwadhwa@users.noreply.github.com>
2023-08-05 16:54:19 -0400

    updated editWarehouse component

--- src/components/editWarehouse/editWarehouse.jsx
+++ src/components/editWarehouse/editWarehouse.jsx
@@ -5,14 +5,21 @@ import backArrow from "../../assets/icons/arrow_back-24px.svg";
 import { useParams } from "react-router";
 import "../editWarehouse/editWarehouse.scss";
 
+//Edit Warehouse Component
+
 function EditWarehouse() {
+  //state
   const { id } = useParams();
   const [formData, setFormData] = useState({});
 
+  //GET request to get array of warehuose
   useEffect(() => {
     const URL = `http://localhost:5050/api/warehouses/${id}`;
     axios.get(URL).then((res) => {
+      //Store Warehouse data in data
       const data = res.data;
+
+      //set FormData to the warehouse object
       setFormData(data[0]);
     });
   }, []);
@@ -29,9 +36,7 @@ function EditWarehouse() {
     console.log(formData);
 
     const URL = `http://localhost:5050/api/warehouses/${id}`;
-    // const created = new Date().toISOString();
-    // const updated = new Date().toISOString();
-    // setFormData({ ...formData, updated_at: updated });
+    //PUT request to write the new info to the database?
     axios
       .put(URL, formData)
       .then((res) => {
@@ -49,29 +54,35 @@ function EditWarehouse() {
         <Link to="/inventories">
           <img src={backArrow} alt="Back Arrow" />
         </Link>
-        <h1>Edit Warehouse Item</h1>
+        <h1>Edit Warehouse</h1>
       </div>
 
       <form action="submit">
         <div className="form__wrap">
           <div className="details__wrap">
-            <h2>W Details</h2>
+            <h2>Warehouse Details</h2>
 
-            <label htmlFor="item__name">W Name</label>
+            <label htmlFor="item__name">Warehouse Name</label>
             <input type="text" name="item_name" value={formData.item_name} />
 
-            <label htmlFor="description">Street Address</label>
+            <label htmlFor="item__name">Street Address</label>
+            <input type="text" name="item_name" value={formData.item_name} />
+            {/* <label htmlFor="description">Street Address</label>
             <textarea
               name="description"
               id="description"
               value={formData.description}
-            ></textarea>
+            ></textarea> */}
 
-            <label htmlFor="category">City</label>
-            <div className="category">{formData.category}</div>
-
+            <label htmlFor="item__name">City</label>
+            <input type="text" name="item_name" value={formData.item_name} />
+            {/* <label htmlFor="category">City</label>
+            <div className="category">{formData.category}</div> */}
+            <label htmlFor="item__name">Country</label>
+            <input type="text" name="item_name" value={formData.item_name} />
+            {/* 
             <label htmlFor="category">Country</label>
-            <div className="category">{formData.category}</div>
+            <div className="category">{formData.category}</div> */}
           </div>
 
           <div className="availability__wrap">
@@ -85,11 +96,11 @@ function EditWarehouse() {
             </div>
 
             <label htmlFor="warehouse">Position</label>
-            <div className="warehouse_name">{formData.warehouse_name}</div>
+            <input type="text" name="item_name" value={formData.item_name} />
             <label htmlFor="warehouse">Phone Number</label>
-            <div className="warehouse_name">{formData.warehouse_name}</div>
-            <label htmlFor="warehouse">email</label>
-            <div className="warehouse_name">{formData.warehouse_name}</div>
+            <input type="text" name="item_name" value={formData.item_name} />
+            <label htmlFor="warehouse">Email</label>
+            <input type="text" name="item_name" value={formData.item_name} />
           </div>
         </div>
 
@@ -100,95 +111,6 @@ function EditWarehouse() {
       </form>
     </section>
   );
-  //   return (
-  //     <div>
-  //       <h1>Edit Warehouse</h1>
-  //       <form onSubmit={handleSubmit}>
-  //         <div className="leftSide">
-  //           <h2>Warehouse Details</h2>
-  //           <div>
-  //             <label htmlFor="warehouse_name">Warehouse Name</label>
-  //             <input
-  //               type="text"
-  //               name="warehouse_name"
-  //               value={formData.warehouse_name}
-  //               onChange={handleChange}
-  //             />
-  //           </div>
-  //           <div>
-  //             <label htmlFor="address">Street Address</label>
-  //             <input
-  //               type="text"
-  //               name="address"
-  //               value={formData.address}
-  //               onChange={handleChange}
-  //             />
-  //           </div>
-  //           <div>
-  //             <label htmlFor="city">City</label>
-  //             <input
-  //               type="text"
-  //               name="city"
-  //               value={formData.city}
-  //               onChange={handleChange}
-  //             />
-  //           </div>
-  //           <div>
-  //             <label htmlFor="country">Country</label>
-  //             <input
-  //               type="text"
-  //               name="country"
-  //               value={formData.country}
-  //               onChange={handleChange}
-  //             />
-  //           </div>
-  //         </div>
-  //         <div className="rightSide">
-  //           <h2>Contact Details</h2>
-  //           <div>
-  //             <label htmlFor="contact_name">Contact Name</label>
-  //             <input
-  //               type="text"
-  //               name="contact_name"
-  //               value={formData.contact_name}
-  //               onChange={handleChange}
-  //             />
-  //           </div>
-  //           <div>
-  //             <label htmlFor="contact_position">Position</label>
-  //             <input
-  //               type="text"
-  //               name="contact_position"
-  //               value={formData.contact_position}
-  //               onChange={handleChange}
-  //             />
-  //           </div>
-  //           <div>
-  //             <label htmlFor="contact_phone">Phone Number</label>
-  //             <input
-  //               type="text"
-  //               name="contact_phone"
-  //               value={formData.contact_phone}
-  //               onChange={handleChange}
-  //             />
-  //           </div>
-  //           <div>
-  //             <label htmlFor="contact_email">Email</label>
-  //             <input
-  //               type="text"
-  //               name="contact_email"
-  //               value={formData.contact_email}
-  //               onChange={handleChange}
-  //             />
-  //           </div>
-  //         </div>
-  //         <Link to="/">
-  //           <button>Cancel</button>
-  //         </Link>
-  //         <button type="submit">Save</button>
-  //       </form>
-  //     </div>
-  //   );
 }
 
 export default EditWarehouse;
