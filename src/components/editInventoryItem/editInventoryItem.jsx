2c66aee817720a94f0c6067c96c7d24124e88afd
AlisaBlanchard <125149081+AlisaBlanchard@users.noreply.github.com>
2023-08-06 13:38:17 -0400

    fix PUT request functionality

--- src/components/editInventoryItem/editInventoryItem.jsx
+++ src/components/editInventoryItem/editInventoryItem.jsx
@@ -5,148 +5,126 @@ import backArrow from "../../assets/icons/arrow_back-24px.svg";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-//Edit Inventory Item Component
-
 function EditInventoryItem() {
-  // State
-  const [newFormData, setNewFormData] = useState({});
-  // const [formData, setFormData] = useState({});
   const [formData, setFormData] = useState({
-    id:'',
+    id: '',
     item_name: '',
     description: '',
     category: '',
     status: '',
-    warehouse_id: '',
+    warehouse_name: '',
     quantity: '',
-
   });
+  const [warehouseData, setWarehouseData] = useState([])
+  const [warehouseId, setWarehouseId] = useState([]);
+
   const [categories, setCategories] = useState([]);
   const [warehouses, setWarehouses] = useState([]);
 
-  //Pull down url
   const { id } = useParams();
 
-  //GET request to get array of inventory items
   useEffect(() => {
-    //GET array of all inventory items
+
+    
+  })
+
+  useEffect(() => {
     const URL = "http://localhost:5050/api/";
 
-    axios
-      .get(`${URL}inventories/${id}`)
-
+    axios.get(`${URL}inventories/${id}`)
       .then((res) => {
-        //Store inventory item in inventoryData
         const inventoryData = res.data;
-
-        console.log(res.data);
-
-        const newObj = {
-          id:inventoryData.id,
+        console.log("Fetched inventory data:", inventoryData);
+        setFormData({
+          id: inventoryData.id,
           item_name: inventoryData.item_name,
           description: inventoryData.description,
           category: inventoryData.category,
           status: inventoryData.status,
-          warehouse_id: inventoryData.warehouse_name,
+          warehouse_name: inventoryData.warehouse_name,
           quantity: inventoryData.quantity,
-        };
-        console.log(newObj);
-
-        setFormData(inventoryData);
+        });
+      })
+      .catch((err) => {
+        console.log("Error fetching inventory data:", err);
       });
-  }, []);
+  }, [id]);
 
-  //GET request
   useEffect(() => {
-    //GET array of all inventories
     const URL = "http://localhost:5050/api/";
 
-    axios
-      .get(`${URL}inventories`)
+    axios.get(`${URL}inventories`)
+    .then((res) => {
+      const inventoryData = res.data;
+      const categoryData = [];
+      const warehouseData = [];
 
-      .then((res) => {
-        //Store inventory array in inventoryData
-        const inventoryData = res.data;
-
-        const categoryData = [];
-        const warehouseData = [];
-
-        //For each object in the array, if category from obj is not included in categoryData array, push category to array
-        inventoryData.forEach((obj) => {
-          const objCategory = obj.category;
-          if (!categoryData.includes(objCategory)) {
-            categoryData.push(objCategory);
-          }
-        });
-
-        //For each object in the array, if warehouse name from obj is not included in warehouseData array, push warehouse to array
-        inventoryData.forEach((obj) => {
-          const objWarehouse = obj.warehouse_name;
-          if (!warehouseData.includes(objWarehouse)) {
-            warehouseData.push(objWarehouse);
-          }
-        });
-
-        //Setting State
-        setCategories(categoryData);
-        setWarehouses(warehouseData);
+      inventoryData.forEach((obj) => {
+        const objCategory = obj.category;
+        if (!categoryData.includes(objCategory)) {
+          categoryData.push(objCategory);
+        }
       });
+
+      inventoryData.forEach((obj) => {
+        const objWarehouse = obj.warehouse_name;
+        if (!warehouseData.includes(objWarehouse)) {
+          warehouseData.push(objWarehouse);
+        }
+      });
+
+      setCategories(categoryData);
+      setWarehouses(warehouseData);
+    });
   }, []);
 
-  //Change handler to take in form changes
-  const itemEditHandler = (e) => {
-    console.log(e.target.value);
-    const { name, value } = e.target.value;
-    setFormData({...formData, [name]: value });
-    // console.log(newFormData);
+  useEffect(() => {
+    const URL = "http://localhost:5050/api/";
 
-    const newObj = {
-      id:formData.id,
-      warehouse_id: formData.warehouse_id,
-      item_name: formData.item_name,
-      description: formData.description,
-      category: formData.category,
-      status: formData.status,
-      quantity: formData.quantity,
+    axios.get(`${URL}warehouses`)
+    .then((res) => {
+      setWarehouseData(res.data);
+      console.log(res.data);
+
+    })
+  }, []);
+
+  const itemEditHandler = (e) => {
+    const { name, value } = e.target;
+  
+    if(name == "warehouse_name") {
+      const findWarehouse = warehouseData.find((obj) => obj.warehouse_name == value)
+      console.log(findWarehouse);
+
+      setWarehouseId(findWarehouse.id);
+      console.log(findWarehouse.id);
+      console.log({warehouse_id: findWarehouse.id})
+
+      setFormData({ ...formData, [name]: value, warehouse_id: findWarehouse.id});
+      console.log(formData);
+      return
     };
 
-    console.log(newObj);
-
-
-    setFormData(newObj);
-
-    // setFormData(setNewFormData);
-
-    console.log(formData);
-    
+    setFormData({ ...formData, [name]: value });
+    console.log("Updated Form Data:", { ...formData, [name]: value });
   };
 
-
-  //Change Handler to submit new form data
   const itemSubmitHandler = (e) => {
     e.preventDefault();
 
-    // PUT request to write the new info to the database
-    axios
-      .put(`http://localhost:5050/api/inventories/${id}`, formData)
+    console.log("Form Data:", formData);
 
+    const URL = "http://localhost:5050/api/";
+    console.log("Submitting form data:", formData);
+    axios
+      .put(`${URL}inventories/${id}`, formData)
       .then((res) => {
         console.log(res.data);
-
-        // const newObj = {
-        //   warehouse_id: newFormData.warehouse_id,
-        //   item_name: newFormData.item_name,
-        //   description: newFormData.description,
-        //   category: newFormData.category,
-        //   status: newFormData.status,
-        //   quantity: res.quantity,
-        // };
-
-    
+        // Handle success or other logic after successful update
       })
-
       .catch((err) => {
         console.log(err);
+        // Handle error if the update fails
       });
   };
 
@@ -182,12 +160,18 @@ function EditInventoryItem() {
 
             <label htmlFor="category">Category</label>
             <div className="category">
-              <select name="category" value={formData.category} id="categories" onChange={itemEditHandler}>
+              <select
+                name="category"
+                value={formData.category}
+                id="categories"
+                onChange={itemEditHandler}
+              >
                 <option value={formData.category}>{formData.category}</option>
                 {categories.map((category) => {
-                  if (category != formData.category) {
+                  if (category !== formData.category) {
                     return <option value={category}>{category}</option>;
                   }
+                  return null;
                 })}
               </select>
             </div>
@@ -203,30 +187,37 @@ function EditInventoryItem() {
                   type="radio"
                   name="status"
                   value="In Stock"
-                  checked={true}
+                  checked={formData.status === "In Stock"}
                   onChange={itemEditHandler}
-                />{" "}
+                />
                 <label htmlFor="in stock">In stock</label>
                 <input
                   type="radio"
                   name="status"
                   value="Out of Stock"
-                  checked={false}
+                  checked={formData.status === "Out of Stock"}
                   onChange={itemEditHandler}
-                />{" "}
+                />
                 <label htmlFor="out of stock">Out of stock</label>
               </div>
             </div>
 
             <label htmlFor="warehouse">Warehouse</label>
             <div className="warehouse__name">
-              <select name="warehouse_id" value={formData.warehouse_name} id="warehouses_dropdown" onChange={itemEditHandler}>
-                <option value={formData.warehouse_name}>{formData.warehouse_name}</option>
+              <select
+                name="warehouse_name"
+                value={formData.warehouse_name}
+                id="warehouses_dropdown"
+                onChange={itemEditHandler}
+              >
+                <option value={formData.warehouse_name}>
+                  {formData.warehouse_name}
+                </option>
                 {warehouses.map((warehouse) => {
-                  if (warehouse != formData.warehouse_name) {
-                    const warehouse_name = warehouse;
-                    return <option value={warehouse_name}>{warehouse_name}</option>;
+                  if (warehouse !== formData.warehouse_name) {
+                    return <option value={warehouse}>{warehouse}</option>;
                   }
+                  return null;
                 })}
               </select>
             </div>
